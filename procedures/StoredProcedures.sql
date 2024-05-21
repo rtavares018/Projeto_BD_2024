b9b50ca1913e20681b9b0fc4654d649fc2a8c109
rtavares018 <reginapratatavares@gmail.com>
2024-05-21 14:13:26 +0100

    update

--- procedures/StoredProcedures.sql
+++ procedures/StoredProcedures.sql
@@ -1,36 +1,40 @@
 
 --Stored procedure que cria um evento--------------------------------
 CREATE PROCEDURE CriarEvento (
-    @id_evento INT,
     @nomeEvento VARCHAR(50),
-    data_hora DATETIME
+    @data_hora DATETIME,
     @localEvento VARCHAR(50), 
-    @descricaoEvento VARCHAR(100))
-    @lotação_máx INT,
+    @descricaoEvento VARCHAR(100),
+    @lotacao_max INT,
     @custo_previsto DECIMAL(10,2)
-
+)
 AS 
 BEGIN 
-
-    DECLARE @verification INT;
+    DECLARE @novo_id INT;
     DECLARE @erro VARCHAR(100);
 
-    SET @verification = (SELECT dbo.checkId_evento(@id_evento))
-    IF (@verification >= 1)
-        RAISEERROR('O id do evento já existe', 16, 1)
-    ELSE
-        BEGIN 
-            DECLARE @id_evento INT; 
-                SET @id_evento = (SELECT dbo.BuscarId_evento(@nomeEvento))
+    BEGIN TRY
+        -- Calcular o próximo id_evento
+        SELECT @novo_id = ISNULL(MAX(id), 0) + 1 FROM EventPro.Evento;
 
-                BEGIN TRY 
-                    INSERT INTO Evento (id, nome, descrição, custo_previsto, localização, lotação_máx, data_hora)
-                    VALUES (@id_evento, @nomeEvento, @descricaoEvento, @custo_previsto, @localEvento, @lotação_máx, @data_hora);
-                END TRY
-                BEGIN CATCH
-                    SET @erro = ERROR_MESSAGE();
-                    RAISEERROR(@erro, 16, 1) 
-                END CATCH 
+         -- Verificar se o nome do evento já existe
+        IF EXISTS (SELECT 1 FROM EventPro.Evento WHERE nome = @nomeEvento)
+        BEGIN
+            RAISERROR('O nome do evento já existe', 16, 1);
+            RETURN;
+        END
+
+        -- Inserir o evento na tabela Evento
+        INSERT INTO EventPro.Evento (id, nome, descrição, custo_previsto, localização, lotação_máx, data_hora)
+        VALUES (@novo_id, @nomeEvento, @descricaoEvento, @custo_previsto, @localEvento, @lotacao_max, @data_hora);
+
+    END TRY
+    BEGIN CATCH
+        SET @erro = ERROR_MESSAGE();
+        RAISERROR(@erro, 16, 1);
+    END CATCH
+END;
+
 -----inacabado----------------------------------------------
 
 
