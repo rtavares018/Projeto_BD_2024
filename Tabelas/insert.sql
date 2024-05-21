534a9c710bd6262665a7c4098df67271d836c128
rtavares018 <reginapratatavares@gmail.com>
2024-05-21 23:10:26 +0100

    dados

--- Tabelas/insert.sql
+++ Tabelas/insert.sql
@@ -1,53 +1,196 @@
 -- Insert data into Pessoa table
-INSERT INTO Pessoa (num_CC, nome, morada, telemovel, data_nasci, email, password) VALUES
-('123456789', 'João Silva', 'Rua A, 123', '912345678', '1980-05-21', 'joao.silva@example.com', 'password123'),
-('987654321', 'Maria Santos', 'Avenida B, 456', '919876543', '1990-08-15', 'maria.santos@example.com', 'password456'),
-('234567890', 'Ana Pereira', 'Rua C, 789', '915678234', '1985-11-30', 'ana.pereira@example.com', 'password789'),
-('345678901', 'Carlos Lima', 'Avenida D, 321', '913456789', '1975-02-10', 'carlos.lima@example.com', 'password101'),
-('456789012', 'Pedro Almeida', 'Rua E, 987', '916789345', '1982-09-18', 'pedro.almeida@example.com', 'password102'),
-('567890123', 'Luis Matos', 'Rua F, 654', '917890456', '1983-03-22', 'luis.matos@example.com', 'password103'),
-('678901234', 'Sara Oliveira', 'Rua G, 321', '918901234', '1987-07-13', 'sara.oliveira@example.com', 'password104'),
-('789012345', 'Miguel Costa', 'Rua H, 876', '919012345', '1991-01-19', 'miguel.costa@example.com', 'password105');
+INSERT INTO EventPro.Pessoa (num_CC, nome, morada, telemovel, data_nasci, email, password) VALUES
+
+---------------ORGANIZADORES----------------
+('123456789', 'João Silva', 'Rua A, 123', '912345678', '1980-05-21', 'joao.silva@gmail.com', 'password123'),
+('987654321', 'Maria Santos', 'Avenida B, 456', '919876543', '1990-08-15', 'maria.santos@gmail.com', 'password456'),
+('234567890', 'Ana Pereira', 'Rua C, 789', '915678234', '1985-11-30', 'ana.pereira@gmail.com', 'password789'),
+('345678901', 'Carlos Lima', 'Avenida D, 321', '913456789', '1975-02-10', 'carlos.lima@gmail.com', 'password101'),
+('456789012', 'Pedro Almeida', 'Rua E, 987', '916789345', '1982-09-18', 'pedro.almeida@gmail.com', 'password102'),
+('567890123', 'Luis Matos', 'Rua F, 654', '917890456', '1983-03-22', 'luis.matos@gmail.com', 'password103'),
+('678901234', 'Sara Oliveira', 'Rua G, 321', '918901234', '1987-07-13', 'sara.oliveira@gmail.com', 'password104'),
+('789012345', 'Miguel Costa', 'Rua H, 876', '919012345', '1991-01-19', 'miguel.costa@gmail.com', 'password105');
+('123456790', 'Rua NNNN, 654', '912345678', 'marta.gomes@example.com', 'password189'),
+('223456781', 'Rua OOOO, 432', '919876543', 'paulo.rodrigues@example.com', 'password190'),
+('334567892', 'Avenida PPPP, 321', '915678234', 'ana.silva@example.com', 'password191'),
+('445678903', 'Rua QQQQ, 876', '913456789', 'tiago.gomes@example.com', 'password192'),
+('556789014', 'Avenida RRRR, 543', '916789345', 'sara.pereira@example.com', 'password193'),
+('667890125', 'Rua SSSS, 210', '917890456', 'ricardo.martins@example.com', 'password194'),
+('778901236', 'Avenida TTTT, 432', '918901234', 'mariana.costa@example.com', 'password195'),
+('889012347', 'Rua UUUU, 876', '919012345', 'bruno.almeida@example.com', 'password196'),
+('990123458', 'Avenida VVVV, 543', '920123456', 'carla.santos@example.com', 'password197'),
+('001234569', 'Rua WWWW, 210', '921234567', 'hugo.oliveira@example.com', 'password198'),
+('112345670', 'Avenida XXXX, 654', '922345678', 'ana.rodrigues@example.com', 'password199'),
+('223456782', 'Rua YYYY, 432', '923456789', 'pedro.ferreira@example.com', 'password200'),
+('334567893', 'Avenida ZZZZ, 321', '924567890', 'ines.lopes@example.com', 'password201'),
+('445678904', 'Rua 1, 876', '925678901', 'ricardo.gomes@example.com', 'password202'),
+('556789015', 'Avenida 2, 543', '926789012', 'marta.silva@example.com', 'password203'),
+('667890126', 'Rua 3, 210', '927890123', 'bruno.pereira@example.com', 'password204'),
+('778901237', 'Avenida 4, 432', '928901234', 'carla.martins@example.com', 'password205'),
+
+---------------PARTICIPANTES----------------
+('889012348', 'Rua 5, 876', '929901235', 'hugo.rodrigues@example.com', 'password206'),
+('990123459', 'Avenida 6, 543', '930901236', 'ana.pereira@example.com', 'password207'),
+('001234570', 'Rua 7, 210', '931901237', 'pedro.gomes@example.com', 'password208'),
+('112345671', 'Avenida 8, 654', '932901238', 'ines.silva@example.com', 'password209'),
+('223456783', 'Rua 9, 432', '933901239', 'ricardo.rodrigues@example.com', 'password210'),
+('334567894', 'Avenida 10, 321', '934901240', 'marta.pereira@example.com', 'password211'),
+('445678905', 'Rua 11, 876', '935901241', 'bruno.martins@example.com', 'password212'),
+('556789016', 'Avenida 12, 543', '936901242', 'carla.pereira@example.com', 'password213'),
+('667890127', 'Rua 13, 210', '937901243', 'hugo.gomes@example.com', 'password214'),
+('778901238', 'Avenida 14, 432', '938901244', 'ana.martins@example.com', 'password215'),
+('889012349', 'Rua 15, 876', '939901245', 'pedro.rodrigues@example.com', 'password216'),
+('990123460', 'Avenida 16, 543', '940901246', 'ines.rodrigues@example.com', 'password217'),
+('001234571', 'Rua 17, 210', '941901247', 'ricardo.silva@example.com', 'password218'),
+('112345672', 'Avenida 18, 654', '942901248', 'marta.santos@example.com', 'password219'),
+('223456784', 'Rua 19, 432', '943901249', 'bruno.silva@example.com', 'password220'),
+('334567895', 'Avenida 20, 321', '944901250', 'carla.rodrigues@example.com', 'password221'),
+('445678906', 'Rua 21, 876', '945901251', 'hugo.martins@example.com', 'password222'),
+('556789017', 'Avenida 22, 543', '946901252', 'ana.rodrigues@example.com', 'password223'),
+('667890128', 'Rua 23, 210', '947901253', 'pedro.santos@example.com', 'password224'),
+('778901239', 'Avenida 24, 432', '948901254', 'ines.silva@example.com', 'password225'),
+('889012350', 'Rua 25, 876', '949901255', 'ricardo.santos@example.com', 'password226'),
+('990123461', 'Avenida 26, 543', '950901256', 'marta.rodrigues@example.com', 'password227'),
+('001234572', 'Rua 26, 210', '951901257', 'bruno.gomes@example.com', 'password228'),
+('112345673', 'Avenida 27, 654', '952901258', 'carla.silva@example.com', 'password229'),
+('223456785', 'Rua 28, 432', '953901259', 'hugo.pereira@example.com', 'password230'),
+('334567896', 'Avenida 29, 321', '954901260', 'ana.martins@example.com', 'password231'),
+('445678907', 'Rua 30, 876', '955901261', 'pedro.oliveira@example.com', 'password232'),
+('556789018', 'Avenida 31, 543', '956901262', 'ines.rodrigues@example.com', 'password233'),
+('667890129', 'Rua 32, 210', '957901263', 'ricardo.martins@example.com', 'password234'),
+('778901240', 'Avenida 33, 432', '958901264', 'marta.almeida@example.com', 'password235'),
+('889012351', 'Rua 34, 876', '959901265', 'bruno.santos@example.com', 'password236'),
+('990123462', 'Avenida 35, 543', '960901266', 'carla.rodrigues@example.com', 'password237'),
+('001234573', 'Rua 36, 210', '961901267', 'hugo.gomes@example.com', 'password238'),
+('112345674', 'Avenida 37, 654', '962901268', 'ana.ferreira@example.com', 'password239'),
+('223456786', 'Rua 38, 432', '963901269', 'pedro.silva@example.com', 'password240'),
+('334567897', 'Avenida 39, 321', '964901270', 'ines.pereira@example.com', 'password241'),
+('445678908', 'Rua 40, 876', '965901271', 'ricardo.oliveira@example.com', 'password242'),
+('556789019', 'Avenida 41, 543', '966901272', 'marta.gomes@example.com', 'password243'),
+('667890130', 'Rua 42, 210', '967901273', 'bruno.rodrigues@example.com', 'password244'),
+('778901241', 'Avenida 43, 432', '968901274', 'carla.martins@example.com', 'password245'),
+('889012352', 'Rua 44, 876', '969901275', 'hugo.santos@example.com', 'password246'),
+('990123463', 'Avenida 45, 543', '970901276', 'ana.rodrigues@example.com', 'password247'),
+('001234574', 'Rua 46, 210', '971901277', 'pedro.pereira@example.com', 'password248'),
+('112345675', 'Avenida 47, 654', '972901278', 'ines.martins@example.com', 'password249'),
+('223456787', 'Rua 48, 432', '973901279', 'ricardo.silva@example.com', 'password250'),
+('334567898', 'Avenida 49, 321', '974901280', 'marta.pereira@example.com', 'password251'),
+('445678909', 'Rua 50, 876', '975901281', 'bruno.oliveira@example.com', 'password252'),
+('556789020', 'Avenida 51, 543', '976901282', 'carla.gomes@example.com', 'password253'),
+('667890131', 'Rua 52, 210', '977901283', 'hugo.rodrigues@example.com', 'password254'),
+('778901242', 'Avenida 53, 432', '978901284', 'ana.santos@example.com', 'password255'),
+('889012353', 'Rua 54, 876', '979901285', 'pedro.martins@example.com', 'password256'),
+('990123464', 'Avenida 55, 543', '980901286', 'ines.rodrigues@example.com', 'password257'),
+('001234575', 'Rua 56, 210', '981901287', 'ricardo.ferreira@example.com', 'password258'),
+('112345676', 'Avenida 57, 654', '982901288', 'marta.silva@example.com', 'password259'),
+('223456788', 'Rua 58, 432', '983901289', 'bruno.pereira@example.com', 'password260'),
+('334567899', 'Avenida 59, 321', '984901290', 'carla.oliveira@example.com', 'password261'),
+('445678910', 'Rua 60, 876', '985901291', 'hugo.gomes@example.com', 'password262'),
+('556789021', 'Avenida 61, 543', '986901292', 'ana.ferreira@example.com', 'password263'),
+('667890132', 'Rua 62, 210', '987901293', 'pedro.silva@example.com', 'password264'),
+('778901243', 'Avenida 63, 432', '988901294', 'ines.martins@example.com', 'password265'),
+('889012354', 'Rua 64, 876', '989901295', 'ricardo.santos@example.com', 'password266'),
+('990123465', 'Avenida 65, 543', '990901296', 'marta.pereira@example.com', 'password267'),
+('001234576', 'Rua 66, 210', '991901297', 'bruno.oliveira@example.com', 'password268'),
+('112345677', 'Avenida 67, 654', '992901298', 'carla.gomes@example.com', 'password269'),
+('223456789', 'Rua 68, 432', '993901299', 'hugo.rodrigues@example.com', 'password270'),
+('334567900', 'Avenida 69, 321', '994901300', 'ana.santos@example.com', 'password271'),
+('445678911', 'Rua 70, 876', '995901301', 'pedro.martins@example.com', 'password272'),
+('556789022', 'Avenida 71, 543', '996901302', 'ines.rodrigues@example.com', 'password273'),
+------------------TRABALHADORES---------------------------
+('667890133', 'Rua 72, 210', '997901303', 'ricardo.ferreira@example.com', 'password274'),
+('778901244', 'Avenida 73, 432', '998901304', 'marta.silva@example.com', 'password275'),
+('889012355', 'Avenida 75, 543', '912901306', 'bruno.pereira@example.com', 'password276'),
+('990123466', 'Rua 76, 210', '913901307', 'carla.oliveira@example.com', 'password277'),
+('001234577', 'Avenida 77, 654', '914901308', 'hugo.gomes@example.com', 'password278'),
+('112345678', 'Rua 78, 432', '915901309', 'ana.ferreira@example.com', 'password279'),
+('223456790', 'Avenida 79, 321', '916901310', 'pedro.silva@example.com', 'password280'),
+('334567901', 'Rua 80, 876', '917901311', 'ines.martins@example.com', 'password281'),
+('445678912', 'Avenida 81, 543', '918901312', 'ricardo.santos@example.com', 'password282'),
+('556789023', 'Rua 82, 210', '919901313', 'marta.pereira@example.com', 'password283'),
+('667890134', 'Avenida 83, 432', '920901314', 'bruno.oliveira@example.com', 'password284'),
+('778901245', 'Rua 84, 876', '921901315', 'carla.gomes@example.com', 'password285'),
+('889012356', 'Avenida 85, 543', '922901316', 'hugo.rodrigues@example.com', 'password286'),
+('990123467', 'Rua 86, 210', '923901317', 'ana.santos@example.com', 'password287'),
+('001234578', 'Avenida 87, 654', '924901318', 'pedro.martins@example.com', 'password288'),
+('112345679', 'Rua 88, 432', '925901319', 'ines.rodrigues@example.com', 'password289'),
+('223456791', 'Avenida 89, 321', '926901320', 'ricardo.ferreira@example.com', 'password290'),
+('334567902', 'Rua 90, 876', '927901321', 'marta.silva@example.com', 'password291'),
+('445678913', 'Avenida 91, 543', '928901322', 'bruno.pereira@example.com', 'password292'),
+('556789024', 'Rua 92, 210', '929901323', 'carla.oliveira@example.com', 'password293'),
+('667890135', 'Avenida 93, 432', '930901324', 'hugo.gomes@example.com', 'password294'),
+('778901246', 'Rua 94, 876', '931901325', 'ana.ferreira@example.com', 'password295'),
+('889012357', 'Avenida 95, 543', '932901326', 'pedro.silva@example.com', 'password296'),
+('990123468', 'Rua 96, 210', '933901327', 'ines.martins@example.com', 'password297'),
+('001234579', 'Avenida 97, 654', '934901328', 'ricardo.santos@example.com', 'password298'),
+('112345680', 'Rua 98, 432', '935901329', 'marta.pereira@example.com', 'password299'),
+('223456792', 'Avenida 99, 321', '936901330', 'bruno.oliveira@example.com', 'password300');
+
+
 
 -- Insert data into Trabalhador table
-INSERT INTO Trabalhador (num_CC, função, id) VALUES
-('234567890', 'Cantor', 1),
-('456789012', 'Segurança', 2);
+INSERT INTO EventPro.Trabalhador (num_CC, função, id, id_evento) VALUES
+('667890133', 'Cantor', 1, 1),
+('778901244', 'Segurança', 2, 2);
 
 -- Insert data into Participante table
-INSERT INTO Participante (num_CC, id) VALUES
-('987654321', 1),
-('345678901', 2),
-('678901234', 3),
-('789012345',4);
-
+INSERT INTO EventPro.Participante (num_CC, id, id_evento) VALUES
+('889012348', 1, 1),
+('990123459', 2, 1),
+('001234570', 3, 2),
+('112345671',4), 2;
 
 
 
 -- Insert data into Organizador table
-INSERT INTO Organizador (num_CC, id, id_evento) VALUES
-('123456789', 1, 1),
-('567890123', 2, 2);
+INSERT INTO EventPro.Organizador (num_CC, id) VALUES
+('123456789', 1),
+('987654321', 2);
+('234567890', 3),
+('345678901', 4),
+('456789012', 5),
+('567890123', 6),
+('678901234', 7),
+('789012345', 8);
+('123456790', 9),
+('223456781', 10),
+('334567892', 11),
+('445678903', 12),
+('556789014', 13),
+('667890125', 14),
+('778901236', 15),
+('889012347', 16),
+('990123458', 17),
+('001234569', 18),
+('112345670', 19),
+('223456782', 20),
+('334567893', 21),
+('445678904', 22),
+('556789015', 23),
+('667890126', 24),
+('778901237', 25)
+
+
 
 -- Insert data into Evento table
-INSERT INTO Evento (id, nome, descrição, custo_previsto, localização, lotação_máx, data_hora) VALUES
-(1, 'Concerto de Verão', 'Concerto ao ar livre com várias bandas.', 1500.00, 'Aveiro', 500, '2024-07-15 20:00:00'),
-(2, 'Feira de Inverno', 'Feira com diversas barraquinhas e atividades.', 2000.00, 'Lisboa', 1000, '2024-12-05 10:00:00');
+INSERT INTO EventPro.Evento (id, nome, descrição, custo_previsto, localização, lotação_máx, data_hora, id_organizador) VALUES
+(1, 'Concerto de Verão', 'Concerto ao ar livre com várias bandas.', 1500.00, 'Aveiro', 500, '2024-07-15 20:00:00', 1),
+(2, 'Feira de Inverno', 'Feira com diversas barraquinhas e atividades.', 2000.00, 'Lisboa', 1000, '2024-12-05 10:00:00', 2);
 
 -- Insert data into Bilhete table
-INSERT INTO Bilhete (id, tipo, preço, id_evento, id_participa) VALUES
+INSERT INTO EventPro.Bilhete (id, tipo, preço, id_evento, id_participa) VALUES
 (1, 'VIP', 50.00, 1, 1),
-(2, 'Regular', 15.00, 1, 3),
+(2, 'Regular', 15.00, 1, 3), 
 (3, 'Regular', 20.00, 2, 2),
 (4, 'VIP', 60.00, 2, 2);
 
 -- Insert data into Colaborador table
-INSERT INTO Colaborador (id, nome_loja, tipo, custo_participação, id_evento) VALUES
+INSERT INTO EventPro.Colaborador (id, nome_loja, tipo, custo_participação, id_evento) VALUES
 (1, 'Loja A', 'Vendedor', 100.00, 1),
 (2, 'Loja B', 'Patrocinador', 500.00, 2);
 
 -- Insert data into Pagamento_Bilhete table
-INSERT INTO Pagamento_Bilhete (id, data, valor, tipo, id_bilhete, id_participa) VALUES
+INSERT INTO EventPro.Pagamento_Bilhete (id, data, valor, tipo, id_bilhete, id_participa) VALUES
 (1, '2024-06-01', 50.00, 'Transferência', 1, 1),
 (2, '2024-06-15', 15.00, 'Cartão de Crédito', 2, 2),
 (3, '2024-06-15', 20.00, 'Cartão de Crédito', 3, 3),
@@ -56,11 +199,11 @@ INSERT INTO Pagamento_Bilhete (id, data, valor, tipo, id_bilhete, id_participa) 
 
 
 -- Insert data into Pagamento_Colaborador table
-INSERT INTO Pagamento_Colaborador (id, data, valor, tipo, id_colabo) VALUES
+INSERT INTO EventPro.Pagamento_Colaborador (id, data, valor, tipo, id_colabo) VALUES
 (1, '2024-06-01', 100.00, 'Transferência', 1),
 (2, '2024-06-15', 500.00, 'Cartão de Crédito', 2);
 
 -- Insert data into Tem table
-INSERT INTO Tem (id_evento, id_colaboradores) VALUES
+INSERT INTO EventPro.Tem (id_evento, id_colaboradores) VALUES
 (1, 1),
 (2, 2);
